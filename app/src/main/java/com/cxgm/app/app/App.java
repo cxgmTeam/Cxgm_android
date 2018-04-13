29cbf2409488310faed0c9b98eab3014bdc6be08
Dean <abcruoshui@sina.com>
2018-04-13 15:50:25 +0800

    添加了一些工具

--- app/src/main/java/com/cxgm/app/app/App.java
+++ app/src/main/java/com/cxgm/app/app/App.java
@@ -7,11 +7,20 @@ import android.provider.Settings;
 import android.text.TextUtils;
 
 import com.alibaba.fastjson.JSON;
+import com.cxgm.app.R;
 import com.cxgm.app.utils.UserManager;
 import com.deanlib.ootb.OotbConfig;
 import com.deanlib.ootb.data.db.DB;
 import com.deanlib.ootb.utils.DLogUtils;
 import com.deanlib.ootb.utils.VersionUtils;
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+import com.scwang.smartrefresh.layout.api.DefaultRefreshFooterCreator;
+import com.scwang.smartrefresh.layout.api.DefaultRefreshHeaderCreator;
+import com.scwang.smartrefresh.layout.api.RefreshFooter;
+import com.scwang.smartrefresh.layout.api.RefreshHeader;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.footer.ClassicsFooter;
+import com.scwang.smartrefresh.layout.header.ClassicsHeader;
 import com.umeng.analytics.MobclickAgent;
 import com.umeng.commonsdk.UMConfigure;
 import com.umeng.message.IUmengRegisterCallback;
@@ -23,9 +32,30 @@ import org.greenrobot.eventbus.EventBus;
 
 public class App extends Application {
 
+    //static 代码段可以防止内存泄露
+    static {
+        //设置全局的Header构建器
+        SmartRefreshLayout.setDefaultRefreshHeaderCreator(new DefaultRefreshHeaderCreator() {
+            @Override
+            public RefreshHeader createRefreshHeader(Context context, RefreshLayout layout) {
+                layout.setPrimaryColorsId(R.color.colorPrimary, android.R.color.white);//全局设置主题颜色
+                return new ClassicsHeader(context);//.setTimeFormat(new DynamicTimeFormat("更新于 %s"));//指定为经典Header，默认是 贝塞尔雷达Header
+            }
+        });
+        //设置全局的Footer构建器
+        SmartRefreshLayout.setDefaultRefreshFooterCreator(new DefaultRefreshFooterCreator() {
+            @Override
+            public RefreshFooter createRefreshFooter(Context context, RefreshLayout layout) {
+                //指定为经典Footer，默认是 BallPulseFooter
+                return new ClassicsFooter(context).setDrawableSize(20);
+            }
+        });
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
+
 
         OotbConfig.init(this,Constants.DEBUG);
 
