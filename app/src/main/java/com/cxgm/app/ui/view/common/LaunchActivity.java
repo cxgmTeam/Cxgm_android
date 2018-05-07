ddfb27eb7c8e8096426bcfe2343f7135a84e50ae
Dean <abcruoshui@sina.com>
2018-05-07 18:00:42 +0800

    coding

--- app/src/main/java/com/cxgm/app/ui/view/common/LaunchActivity.java
+++ app/src/main/java/com/cxgm/app/ui/view/common/LaunchActivity.java
@@ -4,8 +4,13 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 import com.cxgm.app.R;
+import com.cxgm.app.data.io.common.VersionControlReq;
 import com.cxgm.app.ui.base.BaseActivity;
 import com.cxgm.app.ui.view.ViewJump;
+import com.deanlib.ootb.data.io.Request;
+import com.deanlib.ootb.utils.VersionUtils;
+
+import org.xutils.common.Callback;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -28,8 +33,32 @@ public class LaunchActivity extends BaseActivity {
         setContentView(R.layout.activity_launch);
         ButterKnife.bind(this);
 
+        new VersionControlReq(this, VersionUtils.getAppVersionCode() + "").execute(new Request.RequestCallback() {
+            @Override
+            public void onSuccess(Object o) {
+                toMain();
+            }
+
+            @Override
+            public void onError(Throwable ex, boolean isOnCallback) {
+                toMain();
+            }
+
+            @Override
+            public void onCancelled(Callback.CancelledException cex) {
+
+            }
+
+            @Override
+            public void onFinished() {
+
+            }
+        });
+
+    }
+
+    private void toMain() {
         ViewJump.toMain(this);
         finish();
-
     }
 }
